6e2b20b5947c81770d1b7c98db6c60985bae96c0
Patrick Godwin <patrick.godwin@outlook.com>
2023-02-04 18:57:16 -0800

    move and rename a few more materials - make a dirt cell

--- Assets/Scripts/Grid/GridCell.cs
+++ Assets/Scripts/Grid/GridCell.cs
@@ -4,7 +4,18 @@ using UnityEngine;
 
 public abstract class GridCell : MonoBehaviour
 {
-    // todo: should this be a scriptable object?
-    private GameObject _cellAsset;
+    // the x,y coordinate of the current cell
+    protected int _gridX, _gridY;
+
+    // the overall width and height of the grid
+    protected int _gridHeight, _gridWidth;
+
+    public void SetGridDimensions(int gridX, int gridY, int gridWidth, int gridHeight)
+    {
+        this._gridX = gridX;
+        this._gridY = gridY;
+        this._gridWidth = gridWidth;
+        this._gridHeight = gridHeight;
+    }
 
 }
