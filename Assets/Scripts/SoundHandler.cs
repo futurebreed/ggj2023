080514c9765704f2f8a8ef734daf681312b6d51f
hoodzy315 <62417296+hoodzy315@users.noreply.github.com>
2023-02-04 19:22:16 -0800

    sound manager birds go chirp

---
+++ Assets/Scripts/SoundHandler.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundHandler : MonoBehaviour
+{
+    public FMODUnity.EventReference RootGrowth;
+    FMOD.Studio.EventInstance rootGrowth;
+
+    public FMODUnity.EventReference WaterCollection;
+    FMOD.Studio.EventInstance waterCollect;
+
+    public FMODUnity.EventReference HomeSweetHome;
+    FMOD.Studio.EventInstance homeSweetHome;
+
+
+    public string rootState = null;
+    public string gridState = null;
+
+    void Start()
+    {
+        homeSweetHome = FMODUnity.RuntimeManager.CreateInstance(HomeSweetHome);
+        FMODUnity.RuntimeManager.AttachInstanceToGameObject(homeSweetHome, Camera.main.transform);
+        homeSweetHome.start();
+
+        rootGrowth = FMODUnity.RuntimeManager.CreateInstance(RootGrowth);
+        FMODUnity.RuntimeManager.AttachInstanceToGameObject(rootGrowth, Camera.main.transform);
+
+        waterCollect = FMODUnity.RuntimeManager.CreateInstance(WaterCollection);
+        FMODUnity.RuntimeManager.AttachInstanceToGameObject(waterCollect, Camera.main.transform);
+    }
+
+
+    void Update()
+    {
+        if(rootState == "stretching"){
+            FMOD.Studio.PLAYBACK_STATE playbackState;
+            rootGrowth.getPlaybackState(out playbackState);
+            if(playbackState != FMOD.Studio.PLAYBACK_STATE.PLAYING){
+                rootGrowth.start();
+            }
+        } else {
+            rootGrowth.stop(FMOD.Studio.STOP_MODE.IMMEDIATE);
+        }
+
+        if(gridState == "water") {
+            FMOD.Studio.PLAYBACK_STATE playbackState;
+            waterCollect.getPlaybackState(out playbackState);
+            if(playbackState != FMOD.Studio.PLAYBACK_STATE.PLAYING){
+                waterCollect.start();
+            }
+        }
+    }
+}
